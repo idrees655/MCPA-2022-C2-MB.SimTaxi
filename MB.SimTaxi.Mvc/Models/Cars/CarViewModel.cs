e7c040d8ebde92c1e67a534e792bb4b038ecf295
Idrees Haddad <gelnox@live.com>
2022-11-23 18:35:10 +0300

    - Added Fontawesome icons on Cars/Index - Added Display annotation on CarViewModel

--- MB.SimTaxi.Mvc/Models/Cars/CarViewModel.cs
+++ MB.SimTaxi.Mvc/Models/Cars/CarViewModel.cs
@@ -1,12 +1,23 @@
-﻿namespace MB.SimTaxi.Mvc.Models.Cars
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MB.SimTaxi.Mvc.Models.Cars
 {
     public class CarViewModel
     {
         public int Id { get; set; }
+
+
+        [Display(Name = "Plate Number")]
         public string PlateNumber { get; set; }
+
+
         public string Model { get; set; }
         public string Color { get; set; }
+
+        [Display(Name = "First Name")]
         public string DriverFirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string DriverLastName { get; set; }
     }
 }
