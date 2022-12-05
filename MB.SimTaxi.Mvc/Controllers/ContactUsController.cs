222add61f1b5469c3cee76ec26af1217627f9f6b
Idrees Haddad <gelnox@live.com>
2022-12-05 18:45:08 +0300

    - Added contact us controller woth contact us and about us pages - Changed the navbar theme to dark

---
+++ MB.SimTaxi.Mvc/Controllers/ContactUsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MB.SimTaxi.Mvc.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+    }
+}
